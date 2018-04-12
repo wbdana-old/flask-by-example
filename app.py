0fb41beb573ffea81527b507c4611eaaf1025af9
wbdana <william.b.dana@gmail.com>
2018-04-12 15:29:07 -0400

    Initial GitHub commit

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 @app.route('/')
 def hello():
@@ -8,7 +11,7 @@ def hello():
 @app.route('/<name>')
 def hello_name(name):
     return "Hello {}!".format(name)
-
+    
 if __name__ == '__main__':
     app.run()
     
