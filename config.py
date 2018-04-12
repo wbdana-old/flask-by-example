f750682dccdc95ef5f29bef6b0d193a0cb81fed7
wbdana <william.b.dana@gmail.com>
2018-04-12 16:19:22 -0400

    Add postgresql database to staging

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'something-something'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 class ProductionConfig(Config):
     DEBUG = False
